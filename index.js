f11a5050a3331ef088af5d6482d60a0611ecc362
Samuel Hinga <samuelhinga200@gmail.com>
2022-04-24 12:14:54 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record){
+  let winning = record.find(element => element.result === 'W')
+  if(winning){
+    return winning.year;
+  } else {
+    return undefined;
+  }
+}
